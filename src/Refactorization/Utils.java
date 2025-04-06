22375f5fe7b3faab206615302318c43d7fc0c5dc
Benir2005 <beniromenda@gmail.com>
2025-04-06 14:00:50 +0300

    Using streams API to find student details

--- src/Refactorization/Utils.java
+++ src/Refactorization/Utils.java
@@ -8,15 +8,16 @@ public class Utils {
     public static void getStudentDetails(){
         var classPopulation=Integer.parseInt(JOptionPane.showInputDialog("Enter class population"));
         var stdList=new ArrayList<Student>(classPopulation);
-
         for (int i=0;i<classPopulation;i++){
             i++;
-            String name=JOptionPane.showInputDialog("Enter student "+i +"name: ");
+            String name=JOptionPane.showInputDialog("Enter student "+i +" name: ");
             stdList.add(new Student(name));
             i--;
         }
         stdList.stream().forEach(Student::inputDetails);
         stdList.stream().forEach(student-> System.out.println(student));
+        var avg=stdList.stream().map(m->m.getAvg()).reduce(0.0,Double::sum)/stdList.size();
+        System.out.println(avg);
     }
 
 }
