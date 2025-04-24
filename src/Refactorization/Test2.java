4cde4f1f480b339c51bab79c102ea877657173ab
benir-o <beniromenda@gmail.com>
2025-04-24 10:34:03 +0300

    Push attempt

--- src/Refactorization/Test2.java
+++ src/Refactorization/Test2.java
@@ -3,5 +3,7 @@ package Refactorization;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test 2");
+        System.out.println("Test 3");
     }
 }
