7a6ee23a81c58f48a099c0fa339f8cb71da81f56
benir-o <beniromenda@gmail.com>
2025-04-29 12:59:09 +0300

    sets in python

---
+++ Python Data Structures/python_structures/python_sets.py
@@ -0,0 +1,9 @@
+'''
+A set is an unordered mutable collection of unique elements
+Sets are useful when we want to store a group of items
+without any duplicates and don't need to maintain any specific
+order.
+
+They are defined using curly braces {} or the set () constructor, with items
+separated by commas
+'''
