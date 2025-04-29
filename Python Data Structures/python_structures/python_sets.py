df089bab6c2112d7e3f190622742860ef7ae1a64
benir-o <beniromenda@gmail.com>
2025-04-29 13:21:52 +0300

    sets  and queues in python

--- Python Data Structures/python_structures/python_sets.py
+++ Python Data Structures/python_structures/python_sets.py
@@ -8,3 +8,13 @@ They are defined using curly braces {} or the set () constructor, with items
 separated by commas
 '''
 print("Sets can perform mathematical operations")
+
+'''
+A Ques is a linear data structure that utilizes the first in-first out principle. 
+In a queue, the first element that is added is the first one to be removed-just
+like people waiting in a line
+New elements are added at the rear (end), and elements are removed from the front
+(beginning). 
+Queues can be implemented using lists, collections.deque, or linked lists in python.
+
+'''
