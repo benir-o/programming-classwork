c7f1ddfb5355e6f83a92471d17d46da92364eab8
benir-o <beniromenda@gmail.com>
2025-04-29 13:37:28 +0300

    deletion

--- Python Data Structures/python_structures/Hello.py
+++
@@ -1,8 +0,0 @@
-print("Hello World")
-# Creating a dictionary
-student={"name":"Kylian","age":19,"grades":[88,92,77]}
-#We then access a value
-print(student["name"])
-#Modifying a value
-student["age"]=21
-print(student["age"])
