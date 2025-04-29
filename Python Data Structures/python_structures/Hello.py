5f8ac448a9dd5864238f6bfbccbd09f225c26f0f
benir-o <beniromenda@gmail.com>
2025-04-29 12:55:35 +0300

    Dictionaries in Python

--- Python Data Structures/python_structures/Hello.py
+++ Python Data Structures/python_structures/Hello.py
@@ -1,1 +1,8 @@
 print("Hello World")
+# Creating a dictionary
+student={"name":"Kylian","age":19,"grades":[88,92,77]}
+#We then access a value
+print(student["name"])
+#Modifying a value
+student["age"]=21
+print(student["age"])
