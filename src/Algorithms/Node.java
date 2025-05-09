9b5e403876b2f283537fc82fe9388c7f945823c7
benir-o <beniromenda@gmail.com>
2025-05-09 23:13:43 +0300

    Complete Linked list implementation

--- src/Algorithms/Node.java
+++ src/Algorithms/Node.java
@@ -3,33 +3,56 @@ package Algorithms;
 public class Node {
     int data;
     Node next;
+    public Node (){
+
+    }
     public Node(int data){
         this.data=data;
+        this.next=null;
     }
     public String toString(){
-        return "Data: "+data;
+        return ""+data;
     }
-class LinkedList{
-        public static int countNodes(Node head){
-            int count=1;
-            Node current=head;
-            while(current.next!=null){
-                current=current.next;
-                count++;
-            }
-            return count;
+static class LinkedList{
+        Node head;
+    public static int countNodes(Node head){
+        int count=1;
+        Node current=head;
+        while(current.next!=null){
+            current=current.next;
+            count++;
         }
+        return count;
+    }
+    public void appendNode(int data){
+        Node newNode=new Node(data);
+
+        if (head==null){
+            head=newNode;
+        }
+        else{
+            Node currentNode=head;
+            while (currentNode.next!=null){
+                currentNode=currentNode.next;
+            }
+            currentNode.next=newNode;
+        }
+    }
+    public void printNodes(){
+        while (head.next!=null){
+            System.out.print(head+"-->");
+            head=head.next;
+        }
+        System.out.println(head);
+    }
 
 }
 
     public static void main(String[] args) {
-        Node nodeA=new Node(4);
-        Node nodeB=new Node(2);
-        Node nodeC=new Node(3);
-        Node nodeD=new Node(10);
-        nodeA.next=nodeB;
-        nodeB.next=nodeC;
-        nodeC.next=nodeD;
-        System.out.println(LinkedList.countNodes(nodeA));
+        var l1=new LinkedList();
+        l1.appendNode(20);
+        l1.appendNode(30);
+        l1.appendNode(40);
+        l1.printNodes();
     }
 }
