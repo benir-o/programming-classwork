7e1887ca4bf5dfa84d8f6a47de9829d9d425da3b
benir-o <beniromenda@gmail.com>
2025-06-29 18:35:36 +0300

    BubbleSort revision

--- Algorithms/Binary Search/binary_search/BinarySearch.java
+++ Algorithms/Binary Search/binary_search/BinarySearch.java
@@ -2,37 +2,38 @@ import java.lang.reflect.Array;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] array={-2,3,4,7,8,9,11,13};
-        int target=11;
-        BinarySearch.search(array,target);
-        System.out.println(BinarySearch.rotate(array));
+        int[] array = { -2, 3, 4, 7, 8, 9, 11, 13 };
+        int target = 11;
+        BinarySearch searchItem1 = new BinarySearch();
+        int thing = searchItem1.search(array, target);
+        // System.out.println(BinarySearch.rotate(array));
 
     }
-    public static int search(int[] arr,int target){
-        int start=0;
-        int end=arr.length-1;
-        while (start<=end){
-            int mid=(int)Math.floor((start+end)/2);
-            if (arr[mid]==target){
-                System.out.println("Found "+arr[mid]);
+
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (int) Math.floor((start + end) / 2);
+            if (arr[mid] == target) {
+                System.out.println("Found " + arr[mid]);
                 return arr[mid];
-            }
-            else if (arr[mid]>target){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
         return -1;
     }
-    //Rotating an array by a factor of 1.
-    public static int rotate(int[] array){
-        int first=array[0];
-        for (int i=0;i<array.length-1;i++){
-            array[i]=array[i+1];
+
+    // Rotating an array by a factor of 1.
+    public static int rotate(int[] array) {
+        int first = array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
         }
-        array[array.length-1]=first;
-        return array[array.length-1];
+        array[array.length - 1] = first;
+        return array[array.length - 1];
     }
 }
--- Algorithms/Linked lists/src/Node.java
+++ Algorithms/Linked lists/src/Node.java
@@ -1,51 +1,56 @@
 public class Node {
     int data;
     Node next;
-    public Node(int data){
-        this.data=data;
-        this.next=null;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
     }
-    public String toString(){
-        return ""+data;
+
+    public String toString() {
+        return "" + data;
     }
-static class LinkedList{
+
+    static class LinkedList {
         Node head;
-    public static int countNodes(Node head){
-        int count=1;
-        Node current=head;
-        while(current.next!=null){
-            current=current.next;
-            count++;
-        }
-        return count;
-    }
-    public void appendNode(int data){
-        Node newNode=new Node(data);
 
-        if (head==null){
-            head=newNode;
-        }
-        else{
-            Node currentNode=head;
-            while (currentNode.next!=null){
-                currentNode=currentNode.next;
+        public static int countNodes(Node head) {
+            int count = 1;
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+                count++;
             }
-            currentNode.next=newNode;
+            return count;
         }
-    }
-    public void printNodes(){
-        while (head.next!=null){
-            System.out.print(head+"-->");
-            head=head.next;
-        }
-        System.out.println(head);
-    }
 
-}
+        public void appendNode(int data) {
+            Node newNode = new Node(data);
+
+            if (head == null) {
+                head = newNode;
+            } else {
+                Node currentNode = head;
+                while (currentNode.next != null) {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = newNode;
+            }
+        }
+
+        public void printNodes() {
+            while (head.next != null) {
+                System.out.print(head + "-->");
+                head = head.next;
+            }
+            System.out.println(head);
+        }
+
+    }
 
     public static void main(String[] args) {
-        var l1=new LinkedList();
-        //At this point the head node is null
+        var l1 = new LinkedList();
+        // At this point the head node is null
         l1.appendNode(20);
         l1.appendNode(30);
         l1.appendNode(40);
